629809af2c42ca6e01bdcf4f5a059273ad8dc1be
Filipp Nikitin <filipp.nikitin.96@gmail.com>
2022-11-11 02:13:49 -0500

    refactor files, deleted all of the files which are not in the new pipeline

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,1 @@
-from .dehazeformer import dehazeformer_t, dehazeformer_s, dehazeformer_b, dehazeformer_d, dehazeformer_w, \
-    dehazeformer_m, dehazeformer_l, dehazeformer_t_color_space, DehazeFormer
+from .dehazeformer import  DehazeFormer
